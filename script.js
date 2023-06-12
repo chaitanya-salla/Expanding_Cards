eaad485f1eace1ab69c77678f96140e024e735d1
Chaithanya Kumar <salla.chaithanya@outlook.com>
2023-06-12 13:11:48 +0530

    changed event listener

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
-  panel.addEventListener('mouseover', () => {
+  panel.addEventListener('click', () => {
     removeActiveClass();
     panel.classList.add('active');
   });
